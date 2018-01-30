7a920b41c75bd7f782d57c8eb08005c265fad093
jarodyyh <yihong@yihongs-mbp.attlocal.net>
2018-01-29 23:35:05 -0600

    stage 1 final

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
-import {ShotChart} from "./ShotChart";
+import React from 'react';
 import nba from 'nba';
-import {Profile} from "./Profile"
+import { ShotChart } from './ShotChart';
+import { Profile } from "./Profile";
 
 export class Main extends React.Component {
     state = {
-        playerId: nba.findPlayer('Russell Westbrook').playerId
+        playerId: nba.findPlayer('Stephen Curry').playerId,
+        playerInfo: {},
     }
 
-    render () {
+    componentDidMount() {
+        nba.stats.playerInfo({ PlayerID: this.state.playerId }).then((info) => {
+            const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+            console.log(playerInfo);
+            this.setState({
+                playerInfo: playerInfo,
+            });
+        });
+    }
+
+    render() {
         return (
-            <div className='main'>
-                <Profile playerId={this.state.playerId}/>
+            <div className="main">
+                <Profile playerId={this.state.playerId} playerInfo={this.state.playerInfo}/>
                 <ShotChart playerId={this.state.playerId}/>
             </div>
-        )
+        );
     }
 }
